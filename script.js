fec543a6b5215d8fa6e5d8e8a7bffe605670ceea
dxdvostfr <49651963+dxdvostfr@users.noreply.github.com>
2019-07-13 03:04:55 +0200

    v1.14
    -Ajout d'un sélecteur de résolution/qualité pour tous les épisodes (pour l'instant non fonctionnel)
    
    -Fix d'un problème d'affichage (léger décalage de l'image de fond sur certaines pages)
    
    -Fix d'un bug de redirection sur la page /s2/ep2
    
    -Fix d'un problème de titre de page (s4/ep1 affiché comme s3/ep1)
    
    -Lecteurs vidéos divers remplacés par des lecteurs MEGA.
    
    -changement des liens morts du mode nostalgia (s1)

---
+++ script.js
@@ -0,0 +1,14 @@
+var switcher$ = $('.switcher'),          // select element
+    switchTarget$ = $('.switch-target'); // iframe
+
+switcher$.on('change', switchIframeSrc); // event binding
+
+// our functiono to switch the iframe src
+function switchIframeSrc() {
+  // set the 'src' attribute of the iframe
+  // to the value of the selected option
+  switchTarget$.attr('src', switcher$.val());
+}
+
+// call the method on load
+switchIframeSrc();
